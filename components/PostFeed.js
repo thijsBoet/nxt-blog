bf306a59ec43e111dd145a160bf3b5b586ccc1c4
thijsBoet <m.boet@chello.nl>
2022-11-30 01:57:02 +0100

    remote

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -6,28 +6,48 @@ export default function PostFeed({ posts, admin }) {
 		: null;
 }
 
-function PostItem({ posts }) {
-	const wordCount = posts?.content.trim().split(/\s+/g).length;
+function PostItem({ post, admin = false }) {
+	// Naive method to calc word count and read time
+	const wordCount = post?.content.trim().split(/\s+/g).length;
 	const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
 	return (
 		<div className='card'>
-			<Link href={`/${posts.username}`}>
+			<Link href={`/${post.username}`}>
 				<a>
-					<strong>By @{posts.username}</strong>
+					<strong>By @{post.username}</strong>
 				</a>
 			</Link>
-			<Link href={`/${posts.username}/${posts.slug}`}>
+
+			<Link href={`/${post.username}/${post.slug}`}>
 				<h2>
-					<a>{posts.title}</a>
+					<a>{post.title}</a>
 				</h2>
 			</Link>
+
 			<footer>
 				<span>
 					{wordCount} words. {minutesToRead} min read
 				</span>
-				<span className='push-left'>❤️ {posts.heartCount || 0} Hearts</span>
+				<span className='push-left'>💗 {post.heartCount || 0} Hearts</span>
 			</footer>
+
+			{/* If admin view, show extra controls for user */}
+			{admin && (
+				<>
+					<Link href={`/admin/${post.slug}`}>
+						<h3>
+							<button className='btn-blue'>Edit</button>
+						</h3>
+					</Link>
+
+					{post.published ? (
+						<p className='text-success'>Live</p>
+					) : (
+						<p className='text-danger'>Unpublished</p>
+					)}
+				</>
+			)}
 		</div>
 	);
 }
